b4b35ca73ffa63d41a960e306d680fca131d757d
Vladimirova Tetiana <vladimirovatanya1107@gmail.com>
2024-12-16 19:47:54 +0200

    fix font style

--- currency-exchange/src/components/Pages/Converter/ConverterHistory/HistoryComp/HistoryInf.tsx
+++ currency-exchange/src/components/Pages/Converter/ConverterHistory/HistoryComp/HistoryInf.tsx
@@ -4,9 +4,9 @@ function HistoryInf({ date, haveMoney, wantMoney, fromCurrency, toCurrency }: Hi
   return (
     <div className="bg-white flex justify-between items-center shadow w-[392px] pt-[15px] pr-[16px] pb-[15px] pl-[16px] mt-[36px] rounded-[4px] text-[18px] font-roboto">
       <p className="font-normal text-gray-line">{date}</p>
-      <p className="font-normal text-gray">{haveMoney} {fromCurrency}</p>
+      <p className="font-medium text-gray">{haveMoney} {fromCurrency}</p>
       <img src="./icons/arrow-left.svg" alt="arrow-left" />
-      <p className="font-normal text-gray">{wantMoney} {toCurrency}</p>
+      <p className="font-medium text-gray">{wantMoney} {toCurrency}</p>
     </div>
   );
 }
